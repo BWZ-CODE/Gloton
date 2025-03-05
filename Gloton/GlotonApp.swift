2271bd4af8201f3539d83ca11373afe8cd812b52
BWZ-CODE <mmige24@gmail.com>
2025-03-05 17:18:29 -0500

    Initial Commit

---
+++ Gloton/GlotonApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlotonApp.swift
+//  Gloton
+//
+//  Created by Miguel Angel Bohorquez on 5/03/25.
+//
+
+import SwiftUI
+
+@main
+struct GlotonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
